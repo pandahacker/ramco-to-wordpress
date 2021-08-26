2c124d52f4fbbe12675d43d6af66db6535726229
Blake Albertson <albertson.blake@yahoo.com>
2021-08-26 11:37:02 -0400

    forgot to require cron

--- meetingIncremental.js
+++ meetingIncremental.js
@@ -2,6 +2,7 @@ var rp = require('request-promise');
 const fetch = require('node-fetch');
 var moment = require('moment');
 var moment = require('moment-timezone');
+var cron = require('node-cron');
 require('dotenv').config();
 var fs = require('fs');
 var _ = require('lodash');

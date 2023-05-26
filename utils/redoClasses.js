b92b62488a9797a051aa0f4b62d20eb480653c20
Blake Albertson <albertson.blake@yahoo.com>
2023-05-26 12:46:33 -0400

    fix outsider provider

--- utils/redoClasses.js
+++ utils/redoClasses.js
@@ -81,8 +81,15 @@ async function pushClasses() {
                     orderId = _.filter(orderId, (o) => o.id !== '8d6bb524-f1d8-41ad-8c21-ae89d35d4dc3');
 
                     orderId = _.filter(orderId, (o) => o.id !== 'c3102913-ffd4-49d6-9bf6-5f0575b0b635');
-                    
-                    orderId = _.filter(orderId, (o) => o.id !== null);
+
+                    orderId = _.filter(orderId, function(o){
+                        
+                        if (o.id === null){
+                        sendDiscordMessage('Price not found', 'order id has null value type', data.cobalt_classId);
+                        }
+
+                        return o.id !== null;
+                    });
 
                     orderId = _.filter(orderId, (o) => o.status === 1);
 
@@ -115,7 +122,13 @@ async function pushClasses() {
 
                     // console.log(`-------`);
 
-                    data.cobalt_price = data.cobalt_price.slice(0, -2);
+                    if(data.cobalt_price){
+                        data.cobalt_price.slice(0, -2);
+                    }
+
+                    if(data.cobalt_OutsideProvider === 'true'){
+                        data.cobalt_price = ' ';
+                    }
 
                     const tags = data.cobalt_cobalt_tag_cobalt_class.map(function (data) {
 
